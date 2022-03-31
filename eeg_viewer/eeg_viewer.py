1053f50140f918c43cd34924d5e73e4f2aeb1a9f
ricardombrodriguez <ricardorodriguez@ua.pt>
2022-04-01 00:16:32 +0100

    some additional info

--- eeg_viewer/eeg_viewer.py
+++ eeg_viewer/eeg_viewer.py
@@ -15,6 +15,32 @@ sigbufs = np.zeros((n, f.getNSamples()[0])) # a partir daqui já tem a ver com o
 fig = plt.figure()
 ax = plt.axes()
 
+# info sobre o eeg
+print(f.file_info_long())
+
+# Returns the transducer of signal chn (“AgAgCl cup electrodes”, etc.).
+print(f.getTransducer(0))
+
+# Mínimos para o canal 0 - Se não tiver parâmetro retorna uma lista com os mínimos de todos os canais numa matriz
+print(f.getPhysicalMinimum(0))
+
+# Máximos para o canal 10 - Se não tiver parâmetro retorna uma lista com os máximos de todos os canais numa matriz
+print(f.getPhysicalMaximum(10))
+
+# Tempo de início do EEG
+print(f.getStartdatetime())
+
+# Tempo de duração
+print(f.getFileDuration())
+
+# Técnico e paciente (algo que podemos usar no projeto)
+print(f.getPatientName())
+print(f.getTechnician())
+
+
+# Anotações do EEG - caso existam
+for annot in f.readAnnotations():
+    print(annot)
 
 for i in np.arange(n):
     """o 'i' é um inteiro"""
@@ -22,3 +48,5 @@ for i in np.arange(n):
     ax.plot(f.readSignal(i))        # pelos vistos ele está a ler o conteúdo de todos os channels
     plt.show()                      # neste caso em concreto existem 129 channels
                                     # quanto aos plots é semelhante a matlab
+
+f.close()
