8417430e7a4952f93ad5b297b5b620cabc8bcd03
ricardombrodriguez <ricardorodriguez@ua.pt>
2022-04-02 00:50:48 +0100

    PDF generation

--- frontend/beegbrain/src/app/app.component.ts
+++ frontend/beegbrain/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+// @ts-ignore
+import pdfMake from "pdfmake/build/pdfmake";  
+// @ts-ignore
+import pdfFonts from "pdfmake/build/vfs_fonts";  
+pdfMake.vfs = pdfFonts.pdfMake.vfs;  
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'beegbrain';
+
+  title = 'BEEGBRAIN';
+
+  generatePDF() {  
+
+    let document = {  
+      header: 'EEG Report',  
+      table: {
+				body: [
+					['Operator', 'Doctor', 'Patient'],
+					['Rachel Solando', 'Alice Torres', 'Andrew Laedis']
+				]
+			},
+      content: 
+      [
+        'The patient has Alzheimer\'s, since the report demonstrated X, Y and Z.'  ,
+      ],
+    };  
+   
+    pdfMake.createPdf(document).open();  
+  }  
 }
