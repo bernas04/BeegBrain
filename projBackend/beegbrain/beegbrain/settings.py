3b675031a6649683da28752a9583b60c409d5bda
ricardombrodriguez <ricardorodriguez@ua.pt>
2022-05-04 19:22:01 +0100

    EEG upload / receive in backend (pt1)

--- projBackend/beegbrain/beegbrain/settings.py
+++ projBackend/beegbrain/beegbrain/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -90,8 +91,8 @@ WSGI_APPLICATION = 'beegbrain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'beegbrain',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
