3fa6002455c2849b23cb4e9ea13650629b6a91df
joaoreis16 <joaoreis16@ua.pt>
2022-05-07 19:07:31 +0100

    eeg viewer - init

---
+++ projFrontend/beegbrain/node_modules/igniteui-angular/public_api.d.ts
@@ -0,0 +1,129 @@
+/**
+ * Animations
+ */
+export * from './lib/animations/main';
+/**
+ * Directives
+ */
+export * from './lib/core/navigation';
+export * from './lib/directives/autocomplete/autocomplete.directive';
+export * from './lib/directives/button/button.directive';
+export * from './lib/directives/divider/divider.directive';
+export * from './lib/directives/drag-drop/drag-drop.strategy';
+export * from './lib/directives/drag-drop/drag-drop.directive';
+export * from './lib/directives/filter/filter.directive';
+export * from './lib/directives/focus/focus.directive';
+export * from './lib/directives/focus-trap/focus-trap.directive';
+export * from './lib/directives/for-of/for_of.directive';
+export * from './lib/directives/layout/layout.directive';
+export * from './lib/directives/mask/mask.directive';
+export * from './lib/directives/radio/radio-group.directive';
+export * from './lib/directives/ripple/ripple.directive';
+export * from './lib/directives/text-highlight/text-highlight.directive';
+export * from './lib/directives/text-selection/text-selection.directive';
+export * from './lib/directives/template-outlet/template_outlet.directive';
+export * from './lib/directives/toggle/toggle.directive';
+export * from './lib/directives/tooltip';
+export * from './lib/directives/date-time-editor/public_api';
+/**
+ * Data operations
+ */
+export * from './lib/data-operations/data-clone-strategy';
+export * from './lib/data-operations/filtering-expression.interface';
+export * from './lib/data-operations/filtering-expressions-tree';
+export * from './lib/data-operations/filtering-condition';
+export * from './lib/data-operations/filtering-state.interface';
+export * from './lib/data-operations/filtering-strategy';
+export * from './lib/data-operations/pivot-sort-strategy';
+export * from './lib/data-operations/pivot-strategy';
+export * from './lib/data-operations/groupby-expand-state.interface';
+export * from './lib/data-operations/groupby-record.interface';
+export * from './lib/data-operations/groupby-state.interface';
+export * from './lib/data-operations/grouping-expression.interface';
+export * from './lib/data-operations/sorting-strategy';
+export * from './lib/data-operations/paging-state.interface';
+export * from './lib/data-operations/data-util';
+/**
+ * Components
+ */
+export * from './lib/action-strip/public_api';
+export * from './lib/accordion/public_api';
+export * from './lib/avatar/avatar.component';
+export * from './lib/badge/badge.component';
+export * from './lib/banner/public_api';
+export * from './lib/buttonGroup/buttonGroup.component';
+export * from './lib/calendar/public_api';
+export * from './lib/card/card.component';
+export * from './lib/carousel/public_api';
+export * from './lib/checkbox/checkbox.component';
+export * from './lib/chips/public_api';
+export * from './lib/combo/public_api';
+export * from './lib/simple-combo/public_api';
+export * from './lib/date-picker/public_api';
+export * from './lib/dialog/dialog.component';
+export * from './lib/drop-down/public_api';
+export * from './lib/grids/common/enums';
+export * from './lib/grids/common/events';
+export * from './lib/grids/filtering/excel-style/public_api';
+export * from './lib/grids/grid/public_api';
+export * from './lib/grids/tree-grid/public_api';
+export * from './lib/grids/hierarchical-grid/public_api';
+export * from './lib/grids/columns/templates.directive';
+export * from './lib/grids/columns/column.component';
+export * from './lib/grids/columns/column-group.component';
+export * from './lib/grids/columns/column-layout.component';
+export * from './lib/grids/hierarchical-grid/hierarchical-grid.pipes';
+export * from './lib/icon/public_api';
+export * from './lib/input-group/public_api';
+export * from './lib/list/public_api';
+export * from './lib/expansion-panel/public_api';
+export * from './lib/navbar/navbar.component';
+export * from './lib/navigation-drawer/public_api';
+export * from './lib/paginator/public_api';
+export * from './lib/paginator/paginator-interfaces';
+export * from './lib/progressbar/progressbar.common';
+export * from './lib/progressbar/progressbar.component';
+export * from './lib/radio/radio.component';
+export * from './lib/slider/public_api';
+export * from './lib/snackbar/snackbar.component';
+export * from './lib/switch/switch.component';
+export * from './lib/tabs/public_api';
+export * from './lib/tabs/bottom-nav/public_api';
+export * from './lib/tabs/tabs/public_api';
+export * from './lib/time-picker/time-picker.directives';
+export * from './lib/time-picker/time-picker.component';
+export * from './lib/toast/toast.component';
+export * from './lib/select/public_api';
+export * from './lib/splitter/splitter-pane/splitter-pane.component';
+export * from './lib/splitter/splitter.component';
+export * from './lib/splitter/splitter.module';
+export * from './lib/stepper/public_api';
+export * from './lib/date-range-picker/public_api';
+export * from './lib/date-common/public_api';
+export * from './lib/grids/column-actions/column-actions-base.directive';
+export * from './lib/grids/column-actions/column-actions.component';
+export * from './lib/tree/public_api';
+export * from './lib/grids/pivot-grid/public_api';
+/**
+ * Exporter services, classes, interfaces and enums
+ */
+export * from './lib/services/public_api';
+export * from './lib/core/dates';
+export * from './lib/core/density';
+export { CancelableEventArgs, CancelableBrowserEventArgs } from './lib/core/utils';
+export { changei18n, getCurrentResourceStrings, IResourceStrings } from './lib/core/i18n/resources';
+export { ICarouselResourceStrings } from './lib/core/i18n/carousel-resources';
+export { IGridResourceStrings } from './lib/core/i18n/grid-resources';
+export { IPaginatorResourceStrings } from './lib/core/i18n/paginator-resources';
+export { ICalendarResourceStrings } from './lib/core/i18n/calendar-resources';
+export { ITimePickerResourceStrings } from './lib/core/i18n/time-picker-resources';
+export { IDatePickerResourceStrings } from './lib/core/i18n/date-picker-resources';
+export { IDateRangePickerResourceStrings } from './lib/core/i18n/date-range-picker-resources';
+export { IListResourceStrings } from './lib/core/i18n/list-resources';
+export { ITreeResourceStrings } from './lib/core/i18n/tree-resources';
+export { IInputResourceStrings } from './lib/core/i18n/input-resources';
+export { IChipResourceStrings } from './lib/core/i18n/chip-resources';
+export { IActionStripResourceStrings } from './lib/core/i18n/action-strip-resources';
+export { PickerInteractionMode } from './lib/date-common/types';
+export { SplitterType } from './lib/splitter/splitter.component';
+export { GridSelectionRange } from './lib/grids/common/types';
