3df7339b8eca2c35a167edf3017e244bd749e653
Mariana Rosa <marianarosa@ua.pt>
2022-05-09 17:39:08 +0100

    Login page

--- projFrontend/beegbrain/src/app/app-routing.module.ts
+++ projFrontend/beegbrain/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -13,7 +14,10 @@ const routes: Routes = [
 
   { path: 'patients', component: PatientsComponent },
 
-  { path: 'profile', component: ProfileComponent }
+  { path: 'profile', component: ProfileComponent },
+  
+  { path: 'login', component: LoginComponent }
+
 
 
 ];
