27f8e5d20df95f9ef874ef8aea62c55a5f62cd02
ricardombrodriguez <ricardorodriguez@ua.pt>
2022-05-11 15:54:13 +0100

    removing folders

--- projFrontend/beegbrain/node_modules/angular-animations/flippers/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { IFlipInXAnimationOptions, flipInXAnimation, flipInXOnEnterAnimation } from './flip-in-x.animation';
-export { IFlipInYAnimationOptions, flipInYAnimation, flipInYOnEnterAnimation } from './flip-in-y.animation';
-export { IFlipOutXAnimationOptions, flipOutXAnimation, flipOutXOnLeaveAnimation } from './flip-out-x.animation';
-export { IFlipOutYAnimationOptions, flipOutYAnimation, flipOutYOnLeaveAnimation } from './flip-out-y.animation';
-export { flipAnimation, flipOnEnterAnimation } from './flip.animation';
