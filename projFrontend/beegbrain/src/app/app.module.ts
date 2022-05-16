4a0ed1e93cebb17ef2c1a5fe3cf95eb72fdd0688
ricardombrodriguez <ricardorodriguez@ua.pt>
2022-05-16 17:29:32 +0100

    Pull frontend/workspace to main

--- projFrontend/beegbrain/src/app/app.module.ts
+++ projFrontend/beegbrain/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
