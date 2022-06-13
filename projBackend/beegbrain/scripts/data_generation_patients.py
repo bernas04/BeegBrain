558f0bebba8c1def0faa4ff35501a99d67147e5c
bernas04 <joaobernardo0@ua.pt>
2022-06-13 23:14:13 +0100

    patient script

--- projBackend/beegbrain/scripts/data_generation_patients.py
+++ projBackend/beegbrain/scripts/data_generation_patients.py
@@ -3,15 +3,61 @@ from app.models import Patient
 
 
 def run():
-    file =  open('./scripts/patients.json', 'r')
-    allDoctors = file.read().split('\n')
+    p1 = Patient(
+        health_number="123456789",
+        name="João Reis",
+        email="joao@ua.pt",
+        address="Ovar",
+        telephone="961234567",
+        birthday="2001-01-01",
+        gender="M",
+        medical_info="This patient has no health diseases."
+    )
+    p2 = Patient(
+        health_number="987654321",
+        name="Mariana Rosa",
+        email="mariana@ua.pt",
+        address="Ribeira Grande",
+        telephone="963291872",
+        birthday="2001-01-01",
+        gender="F",
+        medical_info="This patient has no health diseases."
+    )
+    p3 = Patient(
+        health_number="543216789",
+        name="João Farias",
+        email="bernas@ua.pt",
+        address="Campia",
+        telephone="965746712",
+        birthday="2001-01-01",
+        gender="M",
+        medical_info="This patient has no health diseases."
+    )
+    p4 = Patient(
+        health_number="678912345",
+        name="Ricardo Rodriguez",
+        email="ricardo@ua.pt",
+        address="Estarreja",
+        telephone="918273645",
+        birthday="2001-01-01",
+        gender="M",
+        medical_info="This patient has no health diseases."
+    )
+    p5 = Patient(
+        health_number="678912847",
+        name="Artur Romão",
+        email="artur@ua.pt",
+        address="Ovar",
+        telephone="931123321",
+        birthday="2001-01-01",
+        gender="M",
+        medical_info="This patient has no health diseases."
+    )
 
-    for doctor in allDoctors:
-        if doctor!='':
-            doctor = json.loads(doctor)
-            
+    lst = [p1,p2,p3,p4,p5]
 
-            d_object = Patient(health_number=doctor['health_number'], name=doctor['name'], email=doctor['email'], address=doctor['address'], telephone=doctor['telephone'], birthday=doctor['birthday'], 
-                gender=doctor['gender'], medical_info=doctor['medical_info'])
-            d_object.save()
+    for i in lst:
+        i.save()
+
+    
     
